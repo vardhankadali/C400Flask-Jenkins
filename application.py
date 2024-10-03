fb59a23d5b40cccc3bd50bb7c0085ed1bdd21d39
Vardhan Kadali <vardhankadali54@gmail.com>
2024-10-03 21:07:23 +0530

    Update application.py

--- application.py
+++ application.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello WORELD!'
+    return 'Hello Jenkins!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
